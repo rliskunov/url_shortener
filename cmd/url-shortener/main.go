9f16f4f708252e5622e48295edb6f460a9e94df3
rliskunov <liskunov.roma.rl@gmail.com>
2025-03-23 12:15:05 +0200

    [no task]: add logic for db

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,12 +1,63 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"url_shortener/internal/config"
+	"url_shortener/internal/lib/logger/sl"
+	"url_shortener/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting url-shortener", slog.String("env", cfg.Env))
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("Failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	id, err := storage.SaveURL("https://google.com", "google")
+	if err != nil {
+		log.Error("Failed to save url", sl.Err(err))
+		os.Exit(1)
+	}
+	log.Info("URL saved", slog.Int64("id", id))
+
+	url, err := storage.GetURL("google")
+	if err != nil {
+		log.Warn("URL not found", sl.Err(err))
+	}
+
+	log.Info("URL found", slog.String("url", url))
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
